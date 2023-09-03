224e77f632dabef87b3c400ecdf5e5922d10efdb
Artur <arturnola2@gmail.com>
2023-09-03 18:22:10 -0300

    Função Artur Feita

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let a = 10
-let b = 20
+console.log(funcaoArtur);
+function calcular(n) {
+    if (n < 0) {
+        return "Não é permitido";
+    } else if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        let trap = 1;
+        for (let i = 2; i <= n; i++) {
+            trap *= i;
+        }
+        return trap;
+    }
+}
 
-let c = a + b
-
-console.log(c)
+let  resultado = calcular(5);
+console.log((resultado));
